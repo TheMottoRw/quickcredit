3ab6ae16c91c7046ce45059d675a1327688dac04
TheMottoRw <hasua.mr@gmail.com>
2019-05-07 22:28:09 +0200

    [feature] user signin controller

---
+++ server/controllers/Users.js
@@ -0,0 +1,47 @@
+// import nodemailer from 'nodemailer';
+import quickcredit from '../models/database';
+import { isEmailExist, generateToken } from '../helpers/userAccount';
+
+export const createUser = (req, res) => {
+  const user = req.body;
+  let response = {};
+  if (!user.firstName || !user.lastName || !user.email || !user.password) {
+    response = {
+      status: 400,
+      data: {
+        error: 'Bad request,all information are required',
+      },
+    };
+  } else if (isEmailExist) {
+    response = {
+      status: 200,
+      data: {
+        message: 'email already exist to other account',
+      },
+    };
+  } else {
+    // increment user id for the next user
+    user.id = quickcredit.users.length + 1;
+    user.token = generateToken();
+    user.status = 'unverfied';
+    user.createdOn = new Date();
+    user.isAdmin = false;
+    // push or add user to an array of users
+    quickcredit.users.push(user);
+    // response generate
+    response = {
+      status: 200,
+      data: {
+        id: user.id,
+        token: user.token,
+        firstName: user.firstName,
+        lastName: user.lastName,
+        email: user.email,
+        password: user.password,
+        status: user.status,
+        createdOn: user.createdOn,
+      },
+    };
+  }
+  res.status(response.status).json(response);
+};
