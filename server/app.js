4cf2d1c7d7d6978f75313c38f378992225b3cc0a
TheMottoRw <hasua.mr@gmail.com>
2019-05-11 13:51:11 +0200

    [chore] update with swagger documentation

--- server/app.js
+++ server/app.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import router from './routes/routes';
+import swaggerUi from 'swagger-ui-express';
+import swaggerDocument from './documentation/swagger.json';
 
 const app = express();
 
@@ -7,6 +9,10 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+router.use('/api/v1/docs', swaggerUi.serve);
+router.get('/api/v1/docs', swaggerUi.setup(swaggerDocument));
+
 app.use(router);
 app.listen(PORT, () => {
   console.log(`Quickcredit app started on http://localhost:${PORT}; press Ctrl-C to terminate.`);
