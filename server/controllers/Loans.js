d7977b258b5c336b419240e6881d87b5d77d771d
TheMottoRw <hasua.mr@gmail.com>
2019-05-11 17:13:26 +0200

    [fix] loan apply missing field

--- server/controllers/Loans.js
+++ server/controllers/Loans.js
@@ -1,42 +1,7 @@
 import quickcredit from '../models/database';
-import { isVierified } from '../helpers/userAccount';
+import { isVerified } from '../helpers/userAccount';
 import { hasLoan, installementCalculator, interestCalculator } from '../helpers/Loans';
 
-export const loadLoans = (req, res) => {
-  if (req.body.status === undefined && req.body.repaid === undefined) {
-    res.json(quickcredit.loans);
-  } else {
-    loadByCriteria(req, res);
-  }
-};
-export const loanById = (req, res) => {
-  const loanInfo = quickcredit.loans;
-  let response = null;
-  if (req.params.id === undefined) {
-    response = {
-      status: 400,
-      data: {
-        message: 'bad request, id must be defined',
-      },
-    };
-  } else {
-    const loanid = req.params.id;
-    const specificLoan = loanInfo.find(loan => loan.id === parseInt(loanid));
-    if (specificLoan === undefined) {
-      response = {
-        status: 200,
-        data: {
-          message: 'no data found',
-        },
-      };
-    } else {
-      response = {
-        status: 200,
-        data: specificLoan,
-      };
-    }
-  }
-  res.status(response.status).json(response);
 export const apply = (req, res) => {
   const loan = req.body;
   let response = null;
@@ -50,13 +15,8 @@ export const apply = (req, res) => {
   } else {
   // increment loan id for the next loan
     const userInfo = quickcredit.users.find(user => user.token === loan.user);
-    loan.id = quickcredit.loans.length + 1;
-    loan.balance = loan.amount;
-    loan.repaid = false;
-    loan.status = 'pending';
-    loan.createdOn = new Date();
     // push or add loan to an array of loans
-    if (!isVierified(loan.user)) {
+    if (!isVerified(loan.user)) {
       response = {
         status: 200,
         data: {
@@ -71,24 +31,32 @@ export const apply = (req, res) => {
         },
       };
     } else {
+      loan.user = userInfo.email;
+      loan.id = quickcredit.loans.length + 1;
+      loan.repaid = false;
+      loan.status = 'pending';
+      loan.createdOn = new Date();
+      loan.interest = interestCalculator(loan.amount);
+      loan.status = 'pending';
+      loan.paymentInstallement = installementCalculator(loan.amount, loan.tenor);
+      loan.balance = parseFloat(loan.amount) + parseFloat(loan.interest);
       quickcredit.loans.push(loan);
       // response generate
       response = {
         status: 200,
         data: {
           id: loan.id,
-          user: userInfo.token,
+          user: loan.email,
           firstName: userInfo.firstName,
           lastName: userInfo.lastName,
-          email: userInfo.email,
           createdOn: new Date(),
           status: 'pending',
           repaid: false,
           tenor: loan.tenor,
-          amount: parseFloat(loan.amount),
-          paymentInstallement: installementCalculator(loan.amount, loan.tenor),
-          balance: parseFloat(loan.amount),
-          interest: interestCalculator(loan.interest),
+          amount: loan.amount,
+          paymentInstallement: loan.paymentInstallement,
+          balance: loan.balance,
+          interest: loan.interest,
         },
       };
     }
