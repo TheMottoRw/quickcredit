13dafc51d76085fff737926f2f161205f700b2fd
TheMottoRw <hasua.mr@gmail.com>
2019-05-14 16:06:12 +0200

    [bug] fix logical error to check user has loan

--- server/helpers/Loans.js
+++ server/helpers/Loans.js
@@ -2,7 +2,8 @@
 import quickcredit from '../models/database';
 
 export const hasLoan = (token) => {
-  const loanIndex = quickcredit.loans.findIndex(loan => loan.user === token && (loan.status === 'pending') || (loan.status === 'approved' && loan.repaid === false));
+  const userInfo = quickcredit.users.find(user => user.token === token);
+  const loanIndex = quickcredit.loans.findIndex(loan => loan.user === userInfo.email && (loan.status === 'pending' || (loan.status === 'approved' && loan.repaid === false)));
   return loanIndex !== -1;
 };
 export const interestCalculator = (amount => ((parseFloat(amount) * 5) / 100).toFixed(1));
